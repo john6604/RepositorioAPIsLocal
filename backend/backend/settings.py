63a898ab1d49ac1e9bd6f1e2a7dbf1d293b10df6
BAJS2001 <bryanjuca82001@gmail.com>
2025-05-10 10:53:39 -0500

    Correction crasheo

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,16 +94,16 @@ DATABASES = {
         ssl_require=not DEBUG,
     )
 }
-if os.environ.get("RAILWAY_ENVIRONMENT") == "production":
-    DATABASES['default'] = {
-        'ENGINE':   'django.db.backends.postgresql',
-        'NAME':     os.environ['PGDATABASE'],               # ej. "railway"
-        'USER':     os.environ['PGUSER'],                   # ej. "postgres"
-        'PASSWORD': os.environ['PGPASSWORD'],               # contraseña larga
-        'HOST':     os.environ['RAILWAY_TCP_PROXY_DOMAIN'], # ej. "nozomi.proxy.rlwy.net"
-        'PORT':     os.environ['RAILWAY_TCP_PROXY_PORT'],   # ej. "10812"
-        'OPTIONS':  {'sslmode': 'require'},
-    }
+#if os.environ.get("RAILWAY_ENVIRONMENT") == "production": //PARA MIGRAR 
+#    DATABASES['default'] = {
+#        'ENGINE':   'django.db.backends.postgresql',
+#        'NAME':     os.environ['PGDATABASE'],               # ej. "railway"
+#        'USER':     os.environ['PGUSER'],                   # ej. "postgres"
+#        'PASSWORD': os.environ['PGPASSWORD'],               # contraseña larga
+#        'HOST':     os.environ['RAILWAY_TCP_PROXY_DOMAIN'], # ej. "nozomi.proxy.rlwy.net"
+#        'PORT':     os.environ['RAILWAY_TCP_PROXY_PORT'],   # ej. "10812"
+#        'OPTIONS':  {'sslmode': 'require'},
+#    }
 
 # ------------------------------------------------------------------
 # Validación de contraseñas
