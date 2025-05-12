dd5cb18c29ce569bf339186ab15aa39d9607b844
jchimbo456 <jchimbo456@gmail.com>
2025-05-12 02:28:46 +0000

    urls.py change

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from apis.views import APIViewSet
+from apis.views import APIViewSet, RegistrarUser
 
 router = routers.DefaultRouter()
 router.register('apis', APIViewSet)
@@ -9,4 +9,5 @@ router.register('apis', APIViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path("api/registraruser/", views.RegistrarUser.as_view(), name="registraruser"),
 ]
