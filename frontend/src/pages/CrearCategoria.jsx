928b3b2f2bd90d139a6f8ac749937efac7653bb4
jchimbo456 <jchimbo456@gmail.com>
2025-05-24 17:42:14 +0000

    Correccion link

--- frontend/src/pages/CrearCategoria.jsx
+++ frontend/src/pages/CrearCategoria.jsx
@@ -28,7 +28,6 @@ const CrearCategoria = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${tokenSesion}`,
         },
         body: JSON.stringify(nuevaCategoria),
       });
