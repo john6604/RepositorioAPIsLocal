956a8a111cdcad03f56932664cc6e9719a9b0536
john6604 <129000296+john6604@users.noreply.github.com>
2026-02-02 08:05:50 -0500

    Add ngrok-skip-browser-warning header for API request

--- frontend/src/pages/CrearCategoria.jsx
+++ frontend/src/pages/CrearCategoria.jsx
@@ -30,6 +30,7 @@ const CrearCategoria = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
+          "ngrok-skip-browser-warning": "true",
         },
         body: JSON.stringify(nuevaCategoria),
       });
