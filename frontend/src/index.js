000ff9fed705c0bbb352aa50b711b2ef29b85e8d
john6604 <129000296+john6604@users.noreply.github.com>
2026-02-02 07:56:33 -0500

    Configure axios for request interceptors and auth
    Add axios configuration for request interceptors and authorization token.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import axios from "axios";
+
+axios.defaults.headers.common["ngrok-skip-browser-warning"] = "true";
+
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token_sesion");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,7 +25,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
